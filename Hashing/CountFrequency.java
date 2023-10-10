a03f5a4e94dcf612eb67af813638247680061c0e
Vjchavan <vijaychavan582000@gmail.com>
2023-10-10 23:26:43 +0530

    Casual output file

---
+++ Hashing/CountFrequency.java
@@ -0,0 +1,26 @@
+package Hashing;
+
+import java.util.Scanner;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int res[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            res[i] = arr[i]++;
+        }
+
+        for (int a:res) {
+            System.out.println(a);
+        }
+
+    }
+}
