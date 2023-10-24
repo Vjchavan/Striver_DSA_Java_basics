672894af8a87cd8693e42c9ab4db5da8166378c9
Vjchavan <vijaychavan582000@gmail.com>
2023-10-24 20:42:39 +0530

    SelectionSortSubArrays sorting

---
+++ SortingAlgos/SelectionSortSubArrays.java
@@ -0,0 +1,38 @@
+package SortingAlgos;
+
+import java.util.Scanner;
+
+public class SelectionSortSubArrays {
+
+    public static void main(String[] args) {
+
+        int maxSum = 0;
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n ; j++) {
+
+                int sum = 0;
+
+
+                for (int k = i; k <=j ; k++) {
+
+                    sum = sum + arr[k];
+                    if (maxSum < sum){
+                        maxSum = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxSum);
+    }
+
+}
