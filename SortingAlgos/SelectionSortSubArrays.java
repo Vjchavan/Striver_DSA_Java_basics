d62c9ae4041dd98d051641dd79a5f0c8afd09696
Vjchavan <vijaychavan582000@gmail.com>
2023-10-27 23:35:56 +0530

    Blank Space

--- SortingAlgos/SelectionSortSubArrays.java
+++ SortingAlgos/SelectionSortSubArrays.java
@@ -11,7 +11,7 @@ public class SelectionSortSubArrays {
         int n = in.nextInt();
 
         int[] arr = new int[n];
-
+        
         for(int i=0;i<arr.length;i++){
             arr[i] = in.nextInt();
         }
