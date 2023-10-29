d1cd7879077f4da114ccc2c837c74bb77f1bf84c
Vjchavan <vijaychavan582000@gmail.com>
2023-10-29 10:13:59 +0530

    Completed Merge Sort

--- SortingAlgos/MergeSort.java
+++ SortingAlgos/MergeSort.java
@@ -1,5 +1,6 @@
 package SortingAlgos;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MergeSort {
@@ -11,33 +12,35 @@ public class MergeSort {
 
         int[] arr = new int[n];
 
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = in.nextInt();
         }
 
-        merge(arr,);
+        mergeSort(arr, 0, n - 1);
+
+        for (int a: arr) {
+            System.out.print(a+" ");
+        }
 
     }
 
-}
-
-    public static void mergeSort(int[] arr, int l, int r){
+    public static void mergeSort(int[] arr, int l, int r) {
         // Write your code here
-        if(l>=r) return;
+        if (l >= r) return;
 
-        int mid = (l+r)/2;
+        int mid = (l + r) / 2;
         mergeSort(arr, l, mid);
-        mergeSort(arr, mid+1, r);
-        merge(arr,l,mid,r);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
 
     }
 
-    public static void merge(int[]arr,int l,int mid,int r){
+    public static void merge(int[] arr, int l, int mid, int r) {
         ArrayList<Integer> temp = new ArrayList<>();
         int left = l;
-        int right = mid+1;
+        int right = mid + 1;
 
-        while(left<=mid && right<=r){
+        while (left <= mid && right <= r) {
             if (arr[left] <= arr[right]) {
                 temp.add(arr[left]);
                 left++;
@@ -47,12 +50,12 @@ public class MergeSort {
             }
         }
 
-        while(left<=mid){
+        while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
 
-        while(right<=r){
+        while (right <= r) {
             temp.add(arr[right]);
             right++;
         }
@@ -60,5 +63,5 @@ public class MergeSort {
         for (int i = l; i <= r; i++) {
             arr[i] = temp.get(i - l);
         }
-
     }
+}
