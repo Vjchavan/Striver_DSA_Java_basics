0aaeb62b372115bfe87d799db03d130fad33f2e4
Vjchavan <vijaychavan582000@gmail.com>
2023-12-02 14:21:54 +0530

    Append Node and Delete Node

--- BasicLinkedList/LL.java
+++ BasicLinkedList/LL.java
@@ -17,8 +17,41 @@ public class LL {
     void printLL(){
         Node temp = head;
         while (temp!=null){
-            System.out.println(temp.value);
+            System.out.print(temp.value+" ");
             temp = temp.next;
+        }
+        System.out.println("Length - "+len);
+    }
+
+    void appendLL(int value){
+        Node node = new Node(value);
+        if (len==0){
+            head = node;
+            tail = node;
+        }
+        else {
+            tail.next = node;
+            tail = node;
+            len++;
+        }
+    }
+
+    void RemoveLast(){
+        if(len==0){
+            return;
+        }
+        Node temp = head;
+        Node pre = head;
+        while(temp.next!=null){
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        len--;
+        if (len==0){
+            head = null;
+            tail = null;
         }
     }
 }
--- BasicLinkedList/Main.java
+++ BasicLinkedList/Main.java
@@ -1,8 +1,20 @@
 package BasicLinkedList;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
         LL ll = new LL(4);
+
+        for (int i=0;i<5;i++){
+            ll.appendLL(in.nextInt());
+        }
+
+        ll.printLL();
+        ll.RemoveLast();
+        System.out.println();
         ll.printLL();
     }
 }
