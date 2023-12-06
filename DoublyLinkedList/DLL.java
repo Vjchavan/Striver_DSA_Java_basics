65344f644467f21f697b25c4ca2d588c5f75d939
Vjchavan <vijaychavan582000@gmail.com>
2023-12-06 21:04:59 +0530

    Implemented get node in DLL

--- DoublyLinkedList/DLL.java
+++ DoublyLinkedList/DLL.java
@@ -9,6 +9,69 @@ public class DLL {
         Node node = new Node(value);
         head = node;
         tail = node;
+        len = 1;
+    }
+
+    void printDLL(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.value+" ");
+            temp = temp.next;
+        }
+    }
+
+    void appendDLL(int value){
+        Node node = new Node(value);
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+        len++;
+    }
+
+    void prependDLL(int value){
+        Node node = new Node(value);
+        head.prev = node;
+        node.next = head;
+        head = node;
+        len++;
+    }
+
+    void removeFirst(){
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        head.prev = null;
+        len--;
+    }
+
+    void removeLast(){
+        Node temp = tail.prev;
+        tail.prev = null;
+        tail = temp;
+        tail.next = null;
+        len--;
+
+    }
+
+    Node getNode(int index){
+        if (index<0 && index>len){
+            return null;
+        }
+        Node temp=head;
+        if (index<len/2){
+            System.out.println("From Head");
+            for (int i=0;i<index;i++){
+                temp = temp.next;
+            }
+        }
+        else {
+            temp = tail;
+            System.out.println("From Tail");
+            for (int i=len-1;i>index;i--){
+                temp = temp.prev;
+            }
+        }
+        return temp;
     }
 
 }
--- DoublyLinkedList/Main.java
+++ DoublyLinkedList/Main.java
@@ -2,6 +2,23 @@ package DoublyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
+        DLL dll = new DLL(1);
+        dll.appendDLL(2);
+        dll.appendDLL(3);
+        dll.appendDLL(4);
+        dll.printDLL();
+        System.out.println();
+        dll.removeLast();
+        dll.printDLL();
+        System.out.println();
+        dll.prependDLL(0);
+        dll.printDLL();
+        System.out.println();
+        dll.removeFirst();
+        dll.printDLL();
+        System.out.println();
+        System.out.println( dll.getNode(0).value);
+        
 
     }
 }
